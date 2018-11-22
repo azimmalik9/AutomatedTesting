d61f96d8977d3dbe5eb5e1006f50df5c591c11b1
azimmalik9 <azim.malik9@gmail.com>
2018-11-22 15:26:18 +0000

    read from file and extent report

---
+++ src/main/java/Utils/Constants.java
@@ -0,0 +1,12 @@
+package Utils;
+
+public class Constants {
+ public static final String URL1 = "http://php.thedemosite.co.uk/adduser.php";
+ 
+ public static final String URL2 = "http://php.thedemosite.co.uk/login.php";
+ 
+ public static final String Path_TestData = "C:\\Users\\Azim\\eclipse-workspace\\DemoSite\\src\\testData\\";
+ 
+ public static final String File_TestData = "TestData.xlsx";
+ 
+}
